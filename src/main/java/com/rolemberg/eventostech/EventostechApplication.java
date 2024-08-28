9a99bef146d9bdb70729965483b431e54b49f82f
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-08-28 01:18:13 -0300

    first

---
+++ src/main/java/com/rolemberg/eventostech/EventostechApplication.java
@@ -0,0 +1,11 @@
+package com.rolemberg.eventostech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventostechApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EventostechApplication.class, args);
+	}
+}
